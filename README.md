c015ecc700e2fb5fa6b7da59988b09037192672e
Alexey Davidovich <adavidovich.dev@gmail.com>
2023-02-01 22:11:43 +0100

    Implement dependency installing/uninstalling tool

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # py-basics
+
+# Commands
+
+$ coverage run -m pytest -s
+
+$ coverage html && open htmlcov/index.html
+
+# Libraries installation process
+$ chmod +x install_dependency.sh
+To install
+$ ./requirements.sh install dependency_name
+To remove
+$ ./requirements.sh uninstall dependency_name
--- requirements.sh
+++ requirements.sh
@@ -1,10 +1,44 @@
 #!/bin/bash
-pip install "$1"
-requirement_item=$(pip freeze | grep "$1")
-if grep -Fxq "$requirement_item" requirements.txt
-then
-  echo "Requirement has been already added to requirements file"
-else
-  echo "$requirement_item" >> requirements.txt
-  git commit -m "Add library: '$requirement_item'"
+
+if [ "$1" != "install" ] && [ "$1" != "uninstall" ]; then
+  echo ERROR: Bad command \'"$1"\'
+  exit 1
 fi
+
+if [ "$2" = "" ]; then
+  echo ERROR: No any requirement has been provided
+  exit 1
+fi
+
+case "$1" in
+install)
+  echo INFO: Installing \'"$2"\'...
+  pip install "$2"
+  requirement_descriptor=$(pip freeze | grep -i "$2")
+  if [ "$requirement_descriptor" = "" ]; then
+    echo "ERROR: Requirement has not been found"
+    exit 1
+  fi
+
+  if grep -Fxq "$requirement_descriptor" requirements.txt; then
+    echo "INFO: Requirement has been already added to requirements file"
+  else
+    echo "$requirement_descriptor" >>requirements.txt
+    git commit -m "Add library: '$requirement_descriptor'"
+  fi
+  ;;
+uninstall)
+  echo Uninstalling \'"$2"\'...
+  requirement_descriptor=$(pip freeze | grep -i "$2")
+  if [ "$requirement_descriptor" = "" ]; then
+    echo "ERROR: Requirement has not been found"
+    exit 1
+  fi
+  pip uninstall "$2" --yes
+  sed -i "/$requirement_descriptor/d" requirements.txt
+  ;;
+*)
+  echo ERROR: unknown command "$1"
+  ;;
+esac
+
